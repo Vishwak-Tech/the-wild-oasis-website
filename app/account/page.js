44d6e8239aa1100f827320edf77143c7533747f6
Vishwak Koleti <koletivishwak84@gmail.com>
2026-06-28 11:33:12 +0530

    bootstrap project with layout, metadata, and initial configuration

---
+++ app/account/page.js
@@ -0,0 +1,12 @@
+
+export const metadata = {
+    title: "Guest Area"
+}
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Accounts Page</h1>
+        </div>
+    )
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,13 @@
+import Counter from "../_components/Counter.js";
+
+export const metadata = {
+    title: "Cabins",
+}
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Cabins Page</h1>
+        </div>
+    )
+}
