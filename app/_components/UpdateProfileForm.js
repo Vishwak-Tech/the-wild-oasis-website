f85989a8d9afb43ed7dc0b2e8d7f30c2ffaedbc5
Vishwak Koleti <koletivishwak84@gmail.com>
2026-07-10 22:13:50 +0530

    feat(reservations): implement reservation editing with Server Actions and loading states

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { countries } from "@/app/_lib/countries";
 import { UpdateGuest } from "../_lib/actions";
 import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 export default function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
@@ -64,22 +65,8 @@ export default function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update Profile</SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold
-    hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating...." : "Update profile"}
-    </button>
   );
 }
